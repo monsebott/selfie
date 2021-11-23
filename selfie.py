d97dd48c3c50b31e9481ba33a2e91d85efcbae8c
monsebott <saguirre98@icloud.com>
2021-11-23 10:09:25 -0800

    changed what i had written

--- selfie.py
+++ selfie.py
@@ -1,3 +1,8 @@
-#!/usr/bin/python
-print ("helloworld")
+from picamera import PiCamera
+from time import sleep
 
+camera = PiCamera()
+camera.start_previe()
+sleep(5)
+camera.capture('/tmp/picture.jpg')
+camera.stop_preview()
