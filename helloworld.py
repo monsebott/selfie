0727ada0dc793e018be7baa9e7b47b9bb5da5a5d
monsebott <91340031+monsebott@users.noreply.github.com>
2021-11-29 11:44:24 -0800

    Update helloworld.py
    need to add button onto pi

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
 from time import sleep
 from picamera import PiCamera
-
+button added
 camera = PiCamera()
 camera.resolution = (1024, 768)
 camera.start_preview()
 # Camera warm-up time
-sleep(2)
+sleep(5)
 camera.capture('foo.jpg')
