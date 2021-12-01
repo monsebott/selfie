4c265af19216934a6263073d59ea947fbdde056f
monsebott <saguirre98@icloud.com>
2021-12-01 10:09:40 -0800

    added button

---
+++ camera.py
@@ -0,0 +1,10 @@
+from gpiozero import button
+next_overlay_btn = Button(23)
+take_pic_btn = Button(25)
+next_overlay_btn.when_pressed = next_overlay
+take_pic_btn.when_pressed = take_picture
+def next_overlay():
+    print("Next overlay")
+
+def take_picture():
+    print("Take a picture")
