00ab6b10500ec9eec27a64a6b2483458f1515d73
monsebott <saguirre98@icloud.com>
2021-12-09 10:06:27 -0800

    add to my code

--- camera.py
+++ camera.py
@@ -1,10 +1,20 @@
-from gpiozero import button
+from gpiozero import Button
+from picamera import PiCamera
+from time import gmtime, strftime
 next_overlay_btn = Button(23)
 take_pic_btn = Button(25)
 next_overlay_btn.when_pressed = next_overlay
 take_pic_btn.when_pressed = take_picture
+camera = PiCamera()
+camera.resolution = (800, 480)
+camera.hflip = True
+camera.start_preview(alpha=128)
+output = strftime("/home/pi/allseeingpi/image-%d-%m %H:%M.png", gmtime())
+input()
 def next_overlay():
     print("Next overlay")
 
 def take_picture():
-    print("Take a picture")
+    camera.capture(output)
+    camera.stop_preview()
+
